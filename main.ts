9d26659e43352f617f3daf8c316bf3f3b870fd33
usysrc <usysrc@usysrc.dev>
2024-02-17 22:19:55 +0100

    refactor: :recycle: refactored and added comments

--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ import { MarkdownView, Plugin, TAbstractFile } from 'obsidian';
 
 export default class RandomTitlePlugin extends Plugin {
 
+	/**
+	 * Registers the new note event when the workspace layout is ready
+	 */
 	async onload() {
 		this.app.workspace.onLayoutReady(() => {
 			this.registerEvent(
@@ -15,12 +18,18 @@ export default class RandomTitlePlugin extends Plugin {
 		})
 	}
 
+	/**
+	 * Handles the creation of a new note.
+	 * @param file - The abstract file to be used for creating the new note.
+	 */
 	async onNewNote(file: TAbstractFile) {
-		console.log("new note")
-
+		// Add a delay of 10 milliseconds
 		await this.delay(10);
+		
+		// Rename the file to a random title
 		await this.app.vault.rename(file, `${file.parent?.path}/${this.generateRandomTitle()}.md`);
 
+		// Set the focus on the new note
 		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
 		if (view) {
 			const editor = view.editor;
@@ -29,7 +38,7 @@ export default class RandomTitlePlugin extends Plugin {
 				editor.setCursor({ line: 1, ch: 0 });
 			}
 		}
-    }
+	}
 
 	async delay(ms: number) {
 		return new Promise(resolve => setTimeout(resolve, ms));
@@ -37,10 +46,6 @@ export default class RandomTitlePlugin extends Plugin {
 
 	generateRandomTitle() {
 		return "New Note " + Date.now()
-	}
-
-	onunload() {
-
 	}
 
 }
